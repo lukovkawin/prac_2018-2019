59a4b3a865fafe8b2cd5526a312366ce2a6b7579
ShDaria <31845182+ShDaria@users.noreply.github.com>
2019-04-05 23:39:57 +0300

    Add files via upload

---
+++ submissions/task2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Nash_equilibrium',
+      version='0.1',
+      description='Nash_equilibrium',
+      license='MIT',
+      packages=['Nash_equilibrium'],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
